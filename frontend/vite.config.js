d5fdd4bb44c0942272992de20940776a2a35a93f
krishnaveni <krishnavenisabbi@gmail.com>
2026-04-13 16:25:33 +0530

    disable minify for debugging

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,10 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    host: "0.0.0.0",
-    port: 3000,
-    allowedHosts: true,
-  },
-});
+  plugins: [react()],
+  base: "/",
+  build: {
+    minify: false   // 👈 VERY IMPORTANT
+  }
+})
