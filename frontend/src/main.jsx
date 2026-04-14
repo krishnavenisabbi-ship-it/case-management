74967a0b273ed435e6b644f683ac64183e68ba4a
krishnaveni <krishnavenisabbi@gmail.com>
2026-04-14 10:34:32 +0530

    correct client id

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -6,8 +6,10 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./index.css";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <GoogleOAuthProvider clientId="400532799045-aqoo74q76bms4o0erh379oou3np727b5.apps.googleusercontent.com">
+
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </GoogleOAuthProvider>
 );
+
