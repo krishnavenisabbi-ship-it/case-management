fed4bdfdaf1af912be8effc9e9ede1d6ce2465b0
krishnaveni <krishnavenisabbi@gmail.com>
2026-04-14 13:00:02 +0530

    fix google auth + dashboard + UI alignment

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,7 +5,7 @@ import { GoogleOAuthProvider } from "@react-oauth/google";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <GoogleOAuthProvider clientId="YOUR_CLIENT_ID">
+  <GoogleOAuthProvider clientId="400532799045-aqoo74q76bms4o0erh379oou3np727b5.apps.googleusercontent.com">
     <App />
   </GoogleOAuthProvider>
 );
