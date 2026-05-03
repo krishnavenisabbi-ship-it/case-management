465fbceb7dc134b6d7cd1a4e7d5b9ff71c079564
krishnaveni <krishnavenisabbi@gmail.com>
2026-05-03 17:23:10 +0530

    Updated home page UI and fixed routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
+import React from "react";
 import { Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
 import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 }
