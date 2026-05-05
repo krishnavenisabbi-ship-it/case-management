3abb57864ce840a5ef2d5b76cf2226967f6f69ae
krishnaveni <krishnavenisabbi@gmail.com>
2026-05-05 11:31:02 +0530

    flowDiagram name placed correctly

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,24 +1,24 @@
-import { Routes, Route } from "react-router-dom";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from "./components/Header";
 import Home from "./pages/Home";
-import Login from "./pages/Login";
 import Dashboard from "./pages/Dashboard";
 import AboutUs from "./pages/AboutUs";
-// temporary pages (if not created yet)
-const Features = () => <h1>Features Page</h1>;
-const Contact = () => <h1>Contact Page</h1>;
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
 
 function App() {
   return (
-   <Header />  
-    <Routes>
-      <Route path="/" element={<Home />} /> 
-	   <Route path="/about" element={<AboutUs />} />
-      <Route path="/login" element={<Login />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/features" element={<Features />} />
-      <Route path="/contact" element={<Contact />} />
-    </Routes>
+    <BrowserRouter>
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/about" element={<AboutUs />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
